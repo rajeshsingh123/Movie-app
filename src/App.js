ee741b56fbae95bc829ad8ba49bb2d24428de631
Rajesh Singh <rajesh.rahul7060@gmail.comm>
2023-04-24 11:18:03 +0530

    Movies app is ready to use

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Banner from './components/Banner';
+import NavBar from './components/NavBar';
+import MoviesList from './components/MoviesList';
+import Fav from './components/Fav';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <BrowserRouter>
+   <NavBar/>
+   <Routes>
+    <Route path="/" element={<><Banner/><MoviesList/></>}/>
+   <Route path="/favourites" element={<Fav/>}/>
+   </Routes>
+   </BrowserRouter>
   );
 }
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import { Component } from "react";
+import { Link } from "react-router-dom";
+
+
+class NavBar extends Component{
+    render(){ 
+        return(  
+        <div style={{display:"flex",padding:'.5rem',color:"blue"}}>
+            <Link to="/" style={{textDecoration:"none"}}><h1>Movies app</h1></Link>
+            <Link to="/favourites" style={{textDecoration:"none"}}> <h2 style={{marginLeft:"2rem",marginTop:".5rem"}}>Favourites</h2></Link>    
+        </div>
+)
+}
+}
+
+export default NavBar
